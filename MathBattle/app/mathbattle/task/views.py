58e1bd57c03166b198910a13fa50bc153585a4ee
Ульяна Еськова <UlyanaEskova@MacBook-Air-Ulana.local>
2019-02-18 15:47:12 +0300

    test

---
+++ MathBattle/app/mathbattle/task/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Task, Solves
+from .forms import NumSolveForm
+
+def task(request, task_id):
+    task = Task.objects.last()
+    if request.method == "POST":
+        form = NumSolveForm(request.POST)
+        if form.is_valid():
+            answer = form.cleaned_data['answer']
+            username = request.user.id
+            solve = Solves(username, answer, True)
+            solve.save()
+        return HttpResponse(render(request, 'contest/task.html', {"text" : task.text, "form" : form}))
+
+
+    else:
+        return HttpResponse(render(request, 'contest/task.html', {"text" : task.text, "form" : NumSolveForm()}))
